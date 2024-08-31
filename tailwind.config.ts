f9768a03f6a178a52eb00f20ae6a6256f0e61767
mxkxx1011 <100355178+mxkxx1011@users.noreply.github.com>
2024-08-31 16:18:07 +0900

    chore: 프로젝트 세팅 (#10)
    * style: 폰트, 컬러 세팅
    
    * docs: husky 수정
    
    * style: shadow 스타일 추가
    
    * chore: @svgr/webpack 설치
    
    * chore: svg 컴포넌트 설정
    
    * chore: 이미지 세팅
    
    * chore: axios 설치
    
    * chore: uuid 설치
    
    * chore: react-beautiful-dnd 설치
    
    * style: css 수정
    
    * chore: 커밋 메시지 husky 설정 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,16 +4,88 @@ const config: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/containers/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/stories/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    screens: {
+      tablet: '768px',
+      desktop: '1200px',
+    },
+    colors: {
+      brand: {
+        primary: 'var(--brand-primary)',
+        secondary: 'var(--brand-secondary)',
+        tertiary: 'var(--brand-tertiary)',
+      },
+      point: {
+        purple: 'var(--point-purple)',
+        blue: 'var(--point-blue)',
+        cyan: 'var(--point-cyan)',
+        pink: 'var(--point-pink)',
+        rose: 'var(--point-rose)',
+        orange: 'var(--point-orange)',
+        yellow: 'var(--point-yellow)',
+      },
+      background: {
+        primary: 'var(--background-primary)',
+        secondary: 'var(--background-secondary)',
+        tertiary: 'var(--background-tertiary)',
+        inverse: 'var(--background-inverse)',
+      },
+      intersection: {
+        inactive: 'var(--intersection-inactive)',
+        hover: 'var(--intersection-hover)',
+        pressed: 'var(--intersection-pressed)',
+        focus: 'var(--intersection-focus)',
+      },
+      border: {
+        primary: 'var(--border-primary)',
+      },
+      text: {
+        primary: 'var(--text-primary)',
+        secondary: 'var(--text-secondary)',
+        tertiary: 'var(--text-tertiary)',
+        default: 'var(--text-default)',
+        inverse: 'var(--text-inverse)',
+        disabled: 'var(--text-disabled)',
+      },
+      status: {
+        danger: 'var(--status-danger)',
+      },
+      icon: {
+        primary: 'var(--icon-primary)',
+        inverse: 'var(--icon-inverse)',
+        brand: 'var(--icon-brand)',
+      },
+    },
+
+    fontSize: {
+      '4xl': ['40px', '48px'],
+      '3xl': ['32px', '38px'],
+      '2xl': ['24px', '28px'],
+      xl: ['20px', '24px'],
+      '2lg': ['18px', '21px'],
+      lg: ['16px', '19px'],
+      md: ['14px', '17px'],
+      sm: ['13px', '16px'],
+      xs: ['12px', '14px'],
+    },
+
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'brand-gradient': 'linear-gradient(to right, #10B981, #A3E635)',
+      },
+      boxShadow: {
+        'shadow-xl': 'var(--shadow-xl)',
       },
     },
+    keyframes: {},
+    animation: {},
   },
   plugins: [],
 };
