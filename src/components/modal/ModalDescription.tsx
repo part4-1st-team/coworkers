33f5d7fa8ea5ebe6900a3fe4d199cb7a96514e03
mxkxx <mxkxx@naver.com>
2024-09-02 22:11:45 +0900

    refactor: ModalDescription 컴포넌트에 dompurify 추가

--- src/components/modal/ModalDescription.tsx
+++ src/components/modal/ModalDescription.tsx
@@ -1,8 +1,23 @@
-function ModalDescription({ description }: { description: string }) {
+import clsx from 'clsx';
+import dompurify from 'dompurify';
+
+function ModalDescription({
+  description,
+  className,
+}: {
+  description: string;
+  className?: string;
+}) {
+  const sanitizer = dompurify.sanitize;
+  const cleanDescription = sanitizer(description);
   return (
-    <p className='text-text-secondary text-md font-medium mt-[8px]'>
-      {description}
-    </p>
+    <p
+      className={clsx(
+        'text-text-secondary text-md font-medium mt-[8px]',
+        className,
+      )}
+      dangerouslySetInnerHTML={{ __html: cleanDescription }}
+    />
   );
 }
 
