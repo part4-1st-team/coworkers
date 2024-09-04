af95995b8431908b5d2424ac3cfe72540c6803a7
Seo-Hyang <hyangaroma@gmail.com>
2024-09-04 22:38:32 +0900

    feat: #18 모바일 사이드 메뉴 구현

---
+++ src/components/sidemenu/SideMenu.tsx
@@ -0,0 +1,26 @@
+import { IconX } from '@/assets/IconList';
+
+function SideMenu() {
+  return (
+    <div>
+      <section className='w-[204px] h-[100vh] bg-background-secondary'>
+        <div className='p-[16px] flex flex-col gap-[32px]'>
+          <button className='w-[100%] flex flex-row-reverse'>
+            {/* TODO 닫기 기능 구현 */}
+            <IconX />
+          </button>
+          <div className='flex flex-col gap-[24px] text-white text-[14px] font-[500]'>
+            {/* TODO 데이터 연동에 따라 목록 갱신 */}
+            <a className='h-[24px]'>경영관리팀</a>
+            <a className='h-[24px]'>프로덕트팀</a>
+            <a className='h-[24px]'>마케팅팀</a>
+            <a className='h-[24px]'>콘텐츠팀</a>
+            <a className='h-[24px]'>자유게시판</a>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default SideMenu;
