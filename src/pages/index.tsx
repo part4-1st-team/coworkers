c074cd25e802fa552f87bc09e7b2eab7814a4979
ohjohwan <whghks1020@naver.com>
2024-09-07 06:16:17 +0900

    feat: index.tsx 컴포넌트 원위치

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,3 @@
-import MemberInfo from '@/components/memberInfo/MemberInfo';
-
 export default function Home() {
-  return <MemberInfo />;
-  // return <main>main page</main>;
+  return <main>main page</main>;
 }
