8b7e55f90b9f33ddfa18798011a91a7998c1e164
mxkxx <mxkxx@naver.com>
2024-09-06 14:04:39 +0900

    chore: 테일윈드 config 파일에 pxr 설정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,9 @@
 import type { Config } from 'tailwindcss';
 
+const pxToRem = require('tailwindcss-preset-px-to-rem');
+
 const config: Config = {
+  presets: [pxToRem],
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
