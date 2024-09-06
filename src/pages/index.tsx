f43343d20d37e88420d9f794228f1a5e16fd29e6
ohjohwan <whghks1020@naver.com>
2024-09-06 18:28:38 +0900

    style: TimePicker 컴포넌트 시간 표시, 스크롤바 디자인 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,3 @@
-import TimePicker from '@/components/timeSelect/TimePicker';
-
 export default function Home() {
-  return <TimePicker />;
-  // return <main>main page</main>;
+  return <main>main page</main>;
 }
