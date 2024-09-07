390890ede094cac0a3bed8e2df80675dec3977cd
mxkxx <mxkxx@naver.com>
2024-09-08 01:58:22 +0900

    feat: 리액트 쿼리 프로바이더 설정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,13 @@ import Header from '@/components/header/Header';
 import '@/styles/globals.css';
 import '../components/calendar/calendar.css';
 import type { AppProps } from 'next/app';
+import ReactQueryProviders from '@/hooks/useReactQuery';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ReactQueryProviders>
       <Header />
       <Component {...pageProps} />
-    </>
+    </ReactQueryProviders>
   );
 }
