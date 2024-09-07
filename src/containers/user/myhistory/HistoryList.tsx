3e3d36272dfaab5109dc4031f37d344d3d7b7d59
mxkxx <mxkxx@naver.com>
2024-09-08 01:59:36 +0900

    refactor: 타입 수정 및 css 수정

--- src/containers/user/myhistory/HistoryList.tsx
+++ src/containers/user/myhistory/HistoryList.tsx
@@ -1,16 +1,18 @@
 import { IconCheckboxActive, IconKebabSmall } from '@/assets/IconList';
 
-function HistoryList({ task }: { task: Task }) {
+function HistoryList({ task }: { task: DoneTask }) {
   const { name } = task;
   return (
-    <div className='w-full flex justify-between items-center bg-background-secondary py-[10px] px-[14px] rounded-[8px]'>
-      <div className='flex gap-[7px] items-center'>
+    <div className='w-full flex justify-between items-center bg-background-secondary py-10 px-14 rounded-8'>
+      <div className='flex gap-7 items-center'>
         <IconCheckboxActive />
         <span className='text-text-primary text-md font-normal line-through'>
           {name}
         </span>
       </div>
-      <IconKebabSmall />
+      <button type='button' aria-label='히스토리 수정 삭제 케밥'>
+        <IconKebabSmall className='cursor-pointer' />
+      </button>
     </div>
   );
 }
