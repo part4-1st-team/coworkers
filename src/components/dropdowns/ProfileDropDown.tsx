abfb958c8fc500ca1be4ecf2070fcbdae1fe651c
ohjohwan <whghks1020@naver.com>
2024-09-09 03:53:56 +0900

    feat: ProfileDropDown 컴포넌트 UI 및 반응형 작업 완료

--- src/components/dropdowns/ProfileDropDown.tsx
+++ src/components/dropdowns/ProfileDropDown.tsx
@@ -1,20 +1,31 @@
 function ProfileDropdown() {
   return (
-    <div className='flex flex-col'>
-      <button type='button' className='text-text-primary'>
+    <div className='flex flex-col border border-border-primary rounded-12 w-120 h-160 desktop:w-135 desktop:h-184 tablet:w-135 tablet:h-184'>
+      <button
+        type='button'
+        className='text-text-primary h-40 text-14 font-400 desktop:h-47 desktop:text-16 tablet:h-47 tablet:16'
+      >
         마이 히스토리
       </button>
-      <button type='button' className='text-text-primary'>
+      <button
+        type='button'
+        className='text-text-primary h-40 text-14 font-400 desktop:h-47 desktop:text-16 tablet:h-47 tablet:16'
+      >
         계정 설정
       </button>
-      <button type='button' className='text-text-primary'>
+      <button
+        type='button'
+        className='text-text-primary h-40 text-14 font-400 desktop:h-47 desktop:text-16 tablet:h-47 tablet:16'
+      >
         팀 참여
       </button>
-      <button type='button' className='text-text-primary'>
-        마이 히스토리
+      <button
+        type='button'
+        className='text-text-primary h-40 text-14 font-400 desktop:h-47 desktop:text-16 tablet:h-47 tablet:16'
+      >
+        로그아웃
       </button>
     </div>
   );
 }
-
 export default ProfileDropdown;
