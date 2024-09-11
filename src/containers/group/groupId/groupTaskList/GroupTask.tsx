ee29dc40c5dbee07b69021227009ba6da5911d2b
Seo-Hyang <hyangaroma@gmail.com>
2024-09-12 05:31:15 +0900

    feat: 그룹 할 일 목록 컴포넌트화, 간편화 된 목 데이터 설정

---
+++ src/containers/group/groupId/groupTaskList/GroupTask.tsx
@@ -0,0 +1,64 @@
+// import { useEffect, useState } from 'react';
+import GroupTaskList from './GroupTaskList';
+
+// NOTE 무한 랜딩 됨... 왜....?
+
+function GroupTask({ Lists }: { Lists: MockTaskLists[] }) {
+  // const [listCount, setListCount] = useState(0);
+  // const [doneCount, setDoneCount] = useState(0);
+  // const [totalCount, setTotalCount] = useState(0);
+  // const [isDone, setIsDone] = useState(false);
+
+  // function countDone({ tasks }: { tasks: MockTasks[] }) {
+  //   setTotalCount(tasks.length);
+
+  //   let cnt = 0;
+
+  //   tasks.map((item) => (item.done === true ? cnt++ : cnt));
+  //   setDoneCount(cnt);
+
+  //   if (doneCount === totalCount) {
+  //     setIsDone(true);
+  //   }
+  // }
+
+  return (
+    <section className='w-full mt-24 flex flex-col gap-16'>
+      <div className='w-full flex justify-between'>
+        <div className='flex gap-8'>
+          <p>할 일 목록</p>
+          <p className='text-text-default'>(4개)</p>
+        </div>
+        <p className='text-brand-primary '>+ 새로운 목록 추가하기</p>
+      </div>
+      {/* TODO 데이터 연동, 설정 클릭 구현, 색상 변경 구현 */}
+      <div className='flex flex-col gap-16'>
+        {Lists.length === 0 ? (
+          <div className='text-14 font-md text-text-default mx-auto mt-64 mb-48 desktop:mb-64'>
+            아직 할 일 목록이 없습니다.
+          </div>
+        ) : (
+          Lists.map((tasklist) => (
+            <>
+              <GroupTaskList
+                key={tasklist.id}
+                doneCount={3}
+                totalCount={5}
+                color='purple'
+                isDone={false}
+              >
+                {tasklist.name}
+              </GroupTaskList>
+            </>
+          ))
+        )}
+
+        <GroupTaskList doneCount={3} totalCount={5} color='point-orange' isDone>
+          리스트 컴포넌트
+        </GroupTaskList>
+      </div>
+    </section>
+  );
+}
+
+export default GroupTask;
---
+++ src/types/Mock.ts
@@ -0,0 +1,24 @@
+//mock data
+interface MockMember {
+  id: number;
+  role: string; //ADMIN | MEMBER
+  userImage: string;
+  userEmail: string;
+  userName: string;
+}
+interface MockTasks {
+  id: number;
+  name: string;
+  done: boolean;
+}
+interface MockTaskLists {
+  id: number;
+  name: string;
+  tasks: MockTasks[];
+}
+interface MockGroup {
+  name: string;
+  id: number;
+  tasklists: MockTaskLists[];
+  members: MockMember[];
+}
