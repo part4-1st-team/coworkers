0611e5ede91f2b409514d19f9afe94a5f6ae426a
ohjohwan <whghks1020@naver.com>
2024-09-11 17:53:20 +0900

    fix: ModifyProfile 확장자 명시

---
+++ src/stories/ModifyProfile.stories.tsx
@@ -0,0 +1,18 @@
+import ModifyProfile from '@/components/profile/ModifyProfile.tsx';
+
+const meta = {
+  title: 'Test/ModifyProfile',
+  component: ModifyProfile,
+};
+
+export default meta;
+
+export const Solid = {};
+
+export const Outline = {};
+
+export const Small = {};
+
+export const Medium = {};
+
+export const Large = {};
