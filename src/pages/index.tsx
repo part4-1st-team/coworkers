dd1c2b3b3e799cdf896ddea75caecf917d04d0e6
mxkxx1011 <100355178+mxkxx1011@users.noreply.github.com>
2024-09-12 13:49:41 +0900

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,3 @@
-import Calendar from '@/components/calendar/Calendar';
-
 export default function Home() {
-  return <Calendar />; // return <main>main page</main>;
+  return <main>main page</main>;
 }
