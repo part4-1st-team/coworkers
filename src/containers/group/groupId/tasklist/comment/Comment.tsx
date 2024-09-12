fd6d9241191f14516419b3328b6f915c715c5a47
mxkxx <mxkxx@naver.com>
2024-09-13 03:24:59 +0900

    feat: 댓글 수정 삭제 기능 구현

--- src/containers/group/groupId/tasklist/comment/Comment.tsx
+++ src/containers/group/groupId/tasklist/comment/Comment.tsx
@@ -1,15 +1,27 @@
 import Button from '@/components/button/button';
 import { deleteTaskComment, patchTaskComment } from '@/services/TaskCommentAPI';
-import { useMutation } from '@tanstack/react-query';
+import getDate from '@/utils/getDate';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { useState } from 'react';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import KebabDropdown from './KebabDropdown';
 
-function Comment() {
+function Comment({ comment }: { comment: Comment }) {
   const [isEditing, setIsEditing] = useState<boolean>(false);
 
-  const taskId = 5;
-  const commentId = 6;
+  const {
+    id: commentId,
+    user,
+    createdAt,
+    updatedAt,
+    content,
+    taskId,
+    userId,
+  } = comment;
+
+  const { nickname } = user;
+
+  const queryClient = useQueryClient();
 
   const handleEditing = () => {
     setIsEditing(true);
@@ -18,13 +30,22 @@ function Comment() {
   const patchTaskCommentMutation = useMutation({
     mutationFn: (content: string) =>
       patchTaskComment(taskId, commentId, content),
-    onSuccess: () => {},
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['getTaskCommentList', taskId],
+      });
+      setIsEditing(false);
+    },
     onError: () => {},
   });
 
   const deleteTaskCommentMutation = useMutation({
     mutationFn: () => deleteTaskComment(taskId, commentId),
-    onSuccess: () => {},
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['getTaskCommentList', taskId],
+      });
+    },
     onError: () => {},
   });
 
@@ -36,7 +57,7 @@ function Comment() {
   const { handleSubmit, register } = useForm<CommentState>({
     mode: 'onSubmit',
     defaultValues: {
-      content: '혹시 관련해서 미팅 오늘 중으로 가능하신가요?',
+      content: content,
     },
   });
 
@@ -74,9 +95,7 @@ function Comment() {
   return (
     <div className='w-full flex flex-col gap-16 border-b border-background-tertiary pb-16 border-opacity-10'>
       <div className='flex justify-between items-center'>
-        <span className='text-md font-normal text-text-primary'>
-          혹시 관련해서 미팅 오늘 중으로 가능하신가요?
-        </span>
+        <span className='text-md font-normal text-text-primary'>{content}</span>
         <KebabDropdown
           handleEdit={handleEditing}
           handleDelete={deleteTaskCommentMutation.mutate}
@@ -85,10 +104,12 @@ function Comment() {
       <div className='flex justify-between items-center'>
         <div className='flex gap-12 items-center'>
           <div className='w-32 h-32 rounded-[9999px] bg-white' />
-          <span className='text-md font-medium text-text-primary'>안해나</span>
+          <span className='text-md font-medium text-text-primary'>
+            {nickname}
+          </span>
         </div>
         <span className='text-md font-normal text-text-secondary'>
-          2024년 5월 26일
+          {getDate(updatedAt)}
         </span>
       </div>
     </div>
