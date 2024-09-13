b81b9aca8b4a81e7eecea3475b6dea097b56fcbb
ohjohwan <161469315+ohjohwan@users.noreply.github.com>
2024-09-13 13:47:45 +0900

    feat/#33 회원가입 기능 구현 (#87)
    * feat: 회원가입 UI 작업
    
    * feat: 회원가입 기능 구현
    
    * fix: 테스트를 위한 index import 컴포넌트 제거
    
    * feat: baseURL을 환경변수로 변경(.env.local)
    
    ---------
    
    Co-authored-by: ohjohwan <whghks1020@naver.com>

--- src/services/Auth.API.ts
+++ src/services/Auth.API.ts
@@ -20,11 +20,14 @@ interface LoginResponse {
   };
 }
 
+// 로그인 API
 async function login(payload: LoginPayload): Promise<LoginResponse> {
   try {
-    const response = await axios.post<LoginResponse>('/auth/signIn', payload, {
-      withCredentials: true,
-    });
+    const response = await axios.post<LoginResponse>(
+      '/auth/signIn',
+      payload,
+      {},
+    );
     // eslint-disable-next-line no-console
     console.log(response);
     return response.data;
@@ -45,3 +48,29 @@ async function login(payload: LoginPayload): Promise<LoginResponse> {
 }
 
 export default login;
+
+// 회원가입 API
+interface SignupData {
+  nickname: string;
+  email: string;
+  password: string;
+  passwordConfirmation: string;
+}
+
+export async function signup(data: SignupData) {
+  try {
+    const response = await axios.post('/auth/signUp', data);
+    // eslint-disable-next-line no-console
+    console.log('회원가입 성공:', response.data);
+    return response.data;
+  } catch (error) {
+    if (isAxiosError(error) && error.response) {
+      // eslint-disable-next-line no-console
+      console.error('회원가입 실패:', error.response?.data || error.message);
+    } else {
+      // eslint-disable-next-line no-console
+      console.error('회원가입 실패:', error);
+    }
+    throw error;
+  }
+}
