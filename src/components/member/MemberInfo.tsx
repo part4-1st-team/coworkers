b738dd337ce8306bade01649fb79fa3bfcaf1a69
Seo-Hyang <82509465+Seo-Hyang@users.noreply.github.com>
2024-09-19 13:53:49 +0900

    feat/#28 Group Page (#96)
    * refactor: 멤버 컴포넌트 스타일 수정, 프롭 추가
    
    * refactor: 그룹 멤버 컴포넌트화, 각 버튼 모달 오픈 추가
    
    * docs: 목 타입 수정
    
    * docs: brand tertiary 색상 수정
    
    * feat: 도넛 차트 구현, 그룹 리포트 컴포넌트화
    
    * feat: 그룹 목록 기어 클릭시 모달 오픈 적용
    
    * refactor: 그룹 페이지에 각 컴포넌트 적용
    
    * refactor: lint에 맞게 수정
    
    * refactor: lint에 맞게 수정
    
    * refactor: react circular progressbar 라이브러리 삭제

--- src/components/member/MemberInfo.tsx
+++ src/components/member/MemberInfo.tsx
@@ -1,31 +1,36 @@
 import React from 'react';
 import Image from 'next/image';
+import IconKebabSmall from '@/assets/icons/ic_kebab_small.svg';
+import useModalStore from '@/stores/ModalStore';
+import ProfileModal from '../modal/ProfileModal';
 
-function MemberInfo() {
+// TODO 프로필 이미지 컴포넌트
+
+function MemberInfo({ member }: { member: IMember }) {
   const defaultProfile: string = '/svgs/ic_member.svg';
-  const verticalEllipsis: string = '/svgs/ic_verticalEllipsis.svg';
+  const { setModalOpen } = useModalStore();
+  const { userName, userEmail } = member;
 
   return (
-    <div className='flex justify-between items-center w-[384px] h-[73px] rounded-[16px] px-[24px] py-[20px]'>
-      <div className='flex flex-start gap-[12px]'>
+    <div className='flex justify-between items-center w-fill h-73 rounded-16 px-24 py-20 bg-background-secondary'>
+      <div className='flex flex-start gap-12'>
         <Image
           src={defaultProfile}
           alt='프로필 이미지'
           width={32}
           height={32}
         />
-        <div className='flex flex-col'>
-          <b className='text-text-primary text-[14px]'>우지은</b>
-          <p className='text-text-default text-[12px]'>Email</p>
+        <div className='flex flex-col gap-2'>
+          <b className='text-text-primary text-md'>{userName}</b>
+          <p className='text-text-default text-xs'>{userEmail}</p>
         </div>
       </div>
-      <button type='button'>
-        <Image
-          src={verticalEllipsis}
-          alt='verticalEllipsis(수직 줄임표)'
-          width={2}
-          height={8}
-        />
+      <button
+        type='button'
+        onClick={() => setModalOpen(<ProfileModal member={member} />)}
+        aria-label='프로필 모달 케밥'
+      >
+        <IconKebabSmall />
       </button>
     </div>
   );
