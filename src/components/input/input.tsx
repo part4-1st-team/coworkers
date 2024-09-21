6ee4987db91ec861a04401e033052478ba978ef3
ohjohwan <whghks1020@naver.com>
2024-09-21 15:44:01 +0900

    feat: Input에 주석 추가

--- src/components/input/input.tsx
+++ src/components/input/input.tsx
@@ -3,7 +3,9 @@ import clsx from 'clsx';
 import Label from './Label';
 
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  // 추가적인 커스텀 클래스 네임을 설정
   className?: string;
+  // 입력 필드에 포함할 자식 노드 설정
   children?: React.ReactNode;
 }
 
@@ -15,12 +17,14 @@ interface InputComponent extends React.ForwardRefExoticComponent<InputProps> {
 // Input 컴포넌트 정의
 const Input = forwardRef<HTMLInputElement, InputProps>(
   ({ children, className = '', ...props }, ref) => {
+    // 기본 클래스들
     const baseClasses =
       'w-full px-16 py-14 rounded-xl bg-background-secondary border border-border-primary text-text-primary font-font-normal';
     const placeholderClasses = 'placeholder-text-default text-lg font-normal';
     const focusClasses =
       'pl-16 focus:border-interaction-focus border focus:outline-none';
 
+    // 조건부 클래스
     const hoverClasses = !props.disabled
       ? 'hover:border-interaction-hover border hover:outline-none'
       : '';
@@ -28,7 +32,7 @@ const Input = forwardRef<HTMLInputElement, InputProps>(
       ? 'cursor-not-allowed placeholder-text-disabled focus:outline-none'
       : '';
 
-    // 전체 클래스명 생성
+    // 전체 클래스명
     const classNames = clsx(
       baseClasses,
       placeholderClasses,
