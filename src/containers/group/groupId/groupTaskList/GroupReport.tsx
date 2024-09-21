16b8b702ff45630d707cbde62ac3de5f8544dd0f
Seo-Hyang <hyangaroma@gmail.com>
2024-09-21 19:00:39 +0900

    refactor: 스타일 수정

--- src/containers/group/groupId/groupTaskList/GroupReport.tsx
+++ src/containers/group/groupId/groupTaskList/GroupReport.tsx
@@ -20,9 +20,9 @@ function GroupReport({ doneCount = 0, totalCount = 0 }: Props) {
           <div className='flex gap-64 items-center'>
             <DoughnutChart percentage={taskPercent} />
             <div className='absolute w-150 h-150 flex flex-col items-center justify-center tablet:hidden '>
-              <p className='text-xs'>오늘</p>
+              <p className='text-xs font-medium'>오늘</p>
               {/* TODO 텍스트 그라데이션 적용 */}
-              <p className='text-20 font-bold'>{taskPercent}%</p>
+              <p className='text-xl font-bold'>{taskPercent}%</p>
             </div>
             <div className='hidden tablet:flex flex-col gap-16'>
               <div className='text-md'>
@@ -30,14 +30,14 @@ function GroupReport({ doneCount = 0, totalCount = 0 }: Props) {
                 <p>진행 상황</p>
               </div>
               {/* TODO 텍스트 그라데이션 적용 (text-transparent bg-clip-text bg-gradient-to-r from-brand-primary to-brand-tertiary) */}
-              <p className='text-[40px] font-[700] '>{taskPercent}%</p>
+              <p className='text-4xl font-[700] '>{taskPercent}%</p>
             </div>
           </div>
           <section className='w-1/2 max-w-400 flex flex-col gap-16'>
             <div className='w-full min-w-130 h-76 rounded-12 bg-background-tertiary p-16 flex justify-between items-center'>
               <div className='flex flex-col gap-6'>
                 <p className='text-xs'>오늘의 할 일</p>
-                <p className='text-24 text-brand-tertiary font-bold'>
+                <p className='text-2xl text-brand-tertiary font-bold'>
                   {totalCount}개
                 </p>
               </div>
@@ -48,7 +48,7 @@ function GroupReport({ doneCount = 0, totalCount = 0 }: Props) {
             <div className='w-full min-w-130 h-76 rounded-12 bg-background-tertiary p-16 flex justify-between items-center'>
               <div className='flex flex-col gap-6'>
                 <p className='text-xs'>한 일</p>
-                <p className='text-24 text-brand-tertiary font-bold'>
+                <p className='text-2xl text-brand-tertiary font-bold'>
                   {doneCount}개
                 </p>
               </div>
