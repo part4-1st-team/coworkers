eb8059b0643e5fbf285b773606896fed75322913
Seo-Hyang <hyangaroma@gmail.com>
2024-09-24 06:57:00 +0900

    refactor: 그룹 생성하기 form 훅 수정, 토스트 처리 추가

--- src/containers/group/createGroup/CreateGroup.tsx
+++ src/containers/group/createGroup/CreateGroup.tsx
@@ -1,52 +1,93 @@
 import Button from '@/components/button/button';
 import ImgUpload from '@/components/imgUpload/ImgUpload';
 import Input from '@/components/input/input';
+import useToast from '@/components/toast/useToast';
 import { postGroup } from '@/services/GroupAPI';
+import useImageMutation from '@/hooks/useImageMutation';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { useRouter } from 'next/router';
-import { FormEvent, useState } from 'react';
+import { FormEvent, useEffect, useState } from 'react';
+import { useForm, Controller, SubmitHandler } from 'react-hook-form';
+import { string } from 'yup';
+import ModifyProfile from '@/components/member/modifyProfile';
+
+interface FormState {
+  name: string;
+}
 
 function CreateGroup() {
+  const { toast } = useToast();
+  const { control, handleSubmit } = useForm<FormState>();
   const [groupName, setGroupName] = useState<string>('');
-  const [imgUrl, setImgUrl] = useState<string | null>(null);
+  const [currentImage, setCurrentImage] = useState<Blob | null>(null);
+  const [preview, setPreview] = useState<string | null>(null);
+  const queryClient = useQueryClient();
   const router = useRouter();
 
-  const handleSubmit = async (event: FormEvent) => {
-    event.preventDefault();
+  const imageMutation = useImageMutation();
 
-    if (!groupName) {
-      alert('팀 이름을 입력해주세요.');
-      return;
-    }
+  const createGroupMutation = useMutation({
+    mutationFn: async (name: string) => {
+      const data: PostGroupRequest = { name: '' };
 
-    try {
-      const response = await postGroup({
-        name: groupName,
-        image: imgUrl,
-      } as PostGroupRequest);
+      if (currentImage !== null) {
+        const formData = new FormData();
+        formData.append('image', currentImage);
+        const uploadedImage = await imageMutation.mutateAsync(currentImage);
+        data.image = uploadedImage.url;
+      }
 
-      // 그룹 생성 후 해당 그룹 페이지로 이동
+      data.name = name;
+
+      return postGroup(data);
+    },
+    onSuccess: (response) => {
+      queryClient.invalidateQueries({
+        queryKey: ['group'],
+      });
+      toast('Success', '팀 생성에 성공했습니다.');
       router.push(`/group/${response.id}`);
-    } catch (error) {
-      console.error('그룹 생성에 실패했습니다.', error);
-      alert('그룹 생성에 실패했습니다.');
-    }
+    },
+    onError: () => {
+      if (!groupName) {
+        toast('Error', '팀 이름을 입력해주세요.');
+      }
+      toast('Error', '팀 생성에 실패했습니다.');
+    },
+  });
+
+  const handleCreateGroup: SubmitHandler<FormState> = (data) => {
+    const { name } = data;
+
+    createGroupMutation.mutateAsync(name);
   };
 
   return (
     <section className='mx-16 tablet:mx-142 desktop:mx-430 mt-132 text-lg text-text-primary'>
       <div className='w-full flex flex-col items-center gap-24'>
         <div className='text-4xl'>팀 생성하기</div>
-        <form className='w-full' onSubmit={handleSubmit}>
+        <form className='w-full' onSubmit={handleSubmit(handleCreateGroup)}>
           <div className='w-64 mb-24'>
             <div className='mb-12'>팀 프로필</div>
-            <ImgUpload setImgUrl={setImgUrl} />
+            <ModifyProfile
+              preview={preview}
+              setImage={setCurrentImage}
+              setPreview={setPreview}
+              group
+            />
           </div>
           <div className='mb-40'>
             <div className='mb-12'>팀 이름</div>
-            <Input
-              placeholder='팀 이름을 입력해주세요.'
-              value={groupName}
-              onChange={(e) => setGroupName(e.target.value)}
+            <Controller
+              name='name'
+              control={control}
+              render={({ field }) => (
+                <Input
+                  placeholder='팀 이름을 입력해주세요.'
+                  value={field.value}
+                  onChange={field.onChange}
+                />
+              )}
             />
           </div>
           <Button type='submit' color='primary' size='lg' className='w-full'>
--- src/containers/group/editGroup/EditGroup.tsx
+++ src/containers/group/editGroup/EditGroup.tsx
@@ -3,83 +3,88 @@ import { useForm, Controller, SubmitHandler } from 'react-hook-form';
 import { useRouter } from 'next/router';
 import Input from '@/components/input/input';
 import Button from '@/components/button/button';
+import useToast from '@/components/toast/useToast';
 import { getGroup, patchGroup } from '@/services/GroupAPI';
+import useImageMutation from '@/hooks/useImageMutation';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import ModifyProfile from '@/components/member/modifyProfile';
 
 interface FormState {
   name: string;
 }
 
 function EditGroup() {
-  const { control, handleSubmit, setValue } = useForm<FormState>();
+  const { toast } = useToast();
+  const { control, handleSubmit } = useForm<FormState>();
   const [group, setGroup] = useState<Group | null>(null); // 그룹 정보를 담을 상태
-  const [currentImageBlob, setCurrentImageBlob] = useState<Blob | null>(null);
+  const [currentImage, setCurrentImage] = useState<Blob | null>(null);
   const [preview, setPreview] = useState<string | null>(null);
+  const queryClient = useQueryClient();
   const router = useRouter();
   const { groupId } = router.query;
 
-  const fetchGroup = async () => {
-    try {
-      const groupData = await getGroup(Number(groupId));
-      setGroup(groupData);
-      setValue('name', groupData.name);
-      setPreview(groupData.image);
-    } catch (error) {
-      //TODO 토스트로 처리하기
-      console.error('그룹 정보를 불러오는 데 실패했습니다.', error);
-    }
+  const imageMutation = useImageMutation();
+
+  const groupSetting = async () => {
+    const currentGroup = getGroup(Number(groupId));
+    setGroup(await currentGroup);
+  };
+
+  const updateGroupMutation = useMutation({
+    mutationFn: async (name: string) => {
+      const data: PatchGroup = {};
+
+      if (currentImage !== null) {
+        const formData = new FormData();
+        formData.append('image', currentImage);
+        const uploadedImage = await imageMutation.mutateAsync(currentImage);
+        data.image = uploadedImage.url;
+      }
+
+      if (group?.name !== name) {
+        data.name = name;
+      }
+
+      return patchGroup(Number(groupId), data);
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['group'],
+      });
+      toast('Success', '팀 설정이 업데이트되었습니다.');
+      router.push(`/group/${groupId}`);
+    },
+    onError: () => {
+      toast('Error', '팀 설정 업데이트에 실패했습니다.');
+    },
+  });
+
+  const handleChangeGroup: SubmitHandler<FormState> = (data) => {
+    const { name } = data;
+
+    updateGroupMutation.mutateAsync(name);
   };
 
   useEffect(() => {
-    if (groupId) {
-      fetchGroup();
+    groupSetting();
+    if (group) {
+      setPreview(group?.image);
     }
-  }, [groupId, setValue]);
-
-  const onSubmit: SubmitHandler<FormState> = async (data) => {
-    if (!groupId) return;
-
-    try {
-      let imgUrl = group?.image;
-
-      // TODO 적용되도록 수정
-      // if (currentImageBlob) {
-      //   const formData = new FormData();
-      //   formData.append('image', currentImageBlob);
-
-      //   const imageResponse = await uploadImage(formData); // 이미지 업로드 API 호출
-      //   imgUrl = imageResponse.url;
-      // }
-
-      await patchGroup(Number(groupId), {
-        name: data.name,
-        image: imgUrl,
-      });
-
-      router.push(`/group/${groupId}`);
-    } catch (error) {
-      // TODO 토스트로 처리하기
-      console.error('그룹 수정에 실패했습니다.', error);
-    }
-  };
-
-  if (!group) {
-    // TODO 토스트로 처리
-    return <div>정보 없음 오류</div>;
-  }
+  }, [groupId]);
 
   return (
     <div className='mt-60 w-full h-full bg-background-primary text-text-primary text-lg px-16 pt-56 font-medium'>
       <section className='w-full flex flex-col gap-24 tablet:w-460 mx-auto'>
         <div className='w-full text-center text-24'>팀 수정하기</div>
-        <form onSubmit={handleSubmit(onSubmit)}>
+        <form onSubmit={handleSubmit(handleChangeGroup)}>
           <section>
             <div className='mb-12'>팀 프로필</div>
-            {/* <ModifyProfile
+            <ModifyProfile
               preview={preview}
-              setImage={setCurrentImageBlob}
+              setImage={setCurrentImage}
               setPreview={setPreview}
               group
-            /> */}
+            />
           </section>
           <section>
             <div className='mb-12'>팀 이름</div>
@@ -88,7 +93,7 @@ function EditGroup() {
               control={control}
               render={({ field }) => (
                 <Input
-                  placeholder='팀 이름을 입력해주세요.'
+                  placeholder={group?.name}
                   value={field.value}
                   onChange={field.onChange}
                 />
