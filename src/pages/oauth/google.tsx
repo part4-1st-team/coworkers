5a3856d3e8347236c920a35662f2588ca841301a
ohjohwan <whghks1020@naver.com>
2024-09-23 12:37:17 +0900

    feat: 구글 간편 로그인 구현

--- src/pages/oauth/google.tsx
+++ src/pages/oauth/google.tsx
@@ -1,7 +1,6 @@
-// google.tsx
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
-import axios from 'axios';
+import axios from '@/libs/axios';
 import useUserStore from '@/stores/userStore';
 
 function GoogleOAuth() {
@@ -11,20 +10,22 @@ function GoogleOAuth() {
 
   useEffect(() => {
     const fetchGoogleToken = async () => {
-      if (code) {
+      if (code && state) {
         try {
           // 백엔드로 code를 전송하여 구글 토큰을 요청
-          const response = await axios.post('/api/auth/google', {
-            code,
-            state,
+          const response = await axios.post('/auth/signIn/google', {
+            state: String(state),
+            redirectUri: 'http://localhost:3000/oauth/google', // 구글에 등록한 리디렉트 URI
+            token: String(code), // URL에서 추출한 code를 token으로 전송
           });
+
           const { user, accessToken, refreshToken } = response.data;
 
           // 유저 정보 및 토큰 저장
           setLogin(user, accessToken, refreshToken);
 
           // 성공적으로 로그인된 후 원하는 페이지로 리디렉션
-          router.push('/group/join-group');
+          router.push('/');
         } catch (error) {
           console.error('Google OAuth 로그인 중 오류 발생:', error);
           // 오류가 발생한 경우 처리 (예: 오류 페이지로 이동)
