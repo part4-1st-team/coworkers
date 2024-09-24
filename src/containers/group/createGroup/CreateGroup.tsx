4a3596467f25633814b2fbc3800e5359785e4c77
Seo-Hyang <hyangaroma@gmail.com>
2024-09-25 03:25:38 +0900

    refactor: header에 맞춰 main-container 스타일 적용

--- src/containers/group/createGroup/CreateGroup.tsx
+++ src/containers/group/createGroup/CreateGroup.tsx
@@ -51,33 +51,35 @@ function CreateGroup() {
   };
 
   return (
-    <div className='mx-16 tablet:mx-142 desktop:mx-430 mt-132 text-lg text-text-primary'>
-      <div className='w-full flex flex-col items-center gap-24'>
-        <div className='text-4xl'>팀 생성하기</div>
-        <form className='w-full' onSubmit={handleSubmit(handleCreateGroup)}>
-          <div className='w-64 mb-24'>
-            <div className='mb-12'>팀 프로필</div>
-            <ImgUpload setImgUrl={setImgUrl} />
-          </div>
-          <div className='mb-40'>
-            <div className='mb-12'>팀 이름</div>
-            <Controller
-              name='name'
-              control={control}
-              render={({ field }) => (
-                <Input
-                  placeholder='팀 이름을 입력해주세요.'
-                  value={field.value}
-                  onChange={field.onChange}
-                />
-              )}
-            />
-          </div>
-          <Button type='submit' color='primary' size='lg' className='w-full'>
-            생성하기
-          </Button>
-        </form>
-        <div>팀 이름은 회사명이나 모임 이름 등으로 설정하면 좋아요.</div>
+    <div className='main-container '>
+      <div className='mx-16 tablet:mx-142 desktop:mx-430 mt-132 text-lg text-text-primary'>
+        <div className='w-full flex flex-col items-center gap-24'>
+          <div className='text-4xl'>팀 생성하기</div>
+          <form className='w-full' onSubmit={handleSubmit(handleCreateGroup)}>
+            <div className='w-64 mb-24'>
+              <div className='mb-12'>팀 프로필</div>
+              <ImgUpload setImgUrl={setImgUrl} />
+            </div>
+            <div className='mb-40'>
+              <div className='mb-12'>팀 이름</div>
+              <Controller
+                name='name'
+                control={control}
+                render={({ field }) => (
+                  <Input
+                    placeholder='팀 이름을 입력해주세요.'
+                    value={field.value}
+                    onChange={field.onChange}
+                  />
+                )}
+              />
+            </div>
+            <Button type='submit' color='primary' size='lg' className='w-full'>
+              생성하기
+            </Button>
+          </form>
+          <div>팀 이름은 회사명이나 모임 이름 등으로 설정하면 좋아요.</div>
+        </div>
       </div>
     </div>
   );
