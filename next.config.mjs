4ea4c75aed560fa60f27c964ac3ba93683e81f2c
mxkxx <mxkxx@naver.com>
2024-09-25 03:11:38 +0900

    chore: firebase 이미지 주소 설정

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,12 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 
