b91129278d75f03f2c7260b62a73ba9e8fb7be43
Seo-Hyang <hyangaroma@gmail.com>
2024-09-25 08:22:37 +0900

    feat: 초대 링크 복사 기능 구현

--- src/components/modal/MemberInviteModal.tsx
+++ src/components/modal/MemberInviteModal.tsx
@@ -1,9 +1,21 @@
 import Button from '@/components/button/button';
 import Modal from './Modal';
+import useToast from '../toast/useToast';
 
-function MemberInviteModal() {
-  const handleLinkCopy = () => {
-    // NOTE: 링크 복사하는 핸들러 함수 작성하기
+interface MemberInviteModalProps {
+  inviteLink: string;
+}
+
+function MemberInviteModal({ inviteLink }: MemberInviteModalProps) {
+  const { toast } = useToast();
+
+  const handleLinkCopy = async () => {
+    try {
+      await navigator.clipboard.writeText(inviteLink);
+      toast('Success', '링크가 복사되었습니다.');
+    } catch (error) {
+      toast('Error', '링크 복사에 실패했습니다.');
+    }
   };
 
   return (
