1bf211a7dbdd562595dd793b04db26f2fd1bd11e
ohjohwan <161469315+ohjohwan@users.noreply.github.com>
2024-09-24 14:18:37 +0900

    feat: 간편 로그인/회원가입 기능 구현 #92 (#124)
    * feat: signUp 스키마 생성 및 회원가입 유효성 검사 구현
    
    * feat: 간편 회원가입 기능 추가
    
    ---------
    
    Co-authored-by: ohjohwan <whghks1020@naver.com>

--- src/constants/validate.ts
+++ src/constants/validate.ts
@@ -1,5 +1,9 @@
+// 비밀번호 유효성 검사식
 export const PASSWORD_EXP =
   /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]).{8,}[^\s]*$/;
 
-// 다른 유효성 검사식 추가
-export const EXP = '';
+// 이메일 유효성 검사식
+export const EMAIL_EXP = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+// 닉네임 유효성 검사식
+export const NICKNAME_EXP = /^[a-zA-Z0-9가-힣]{2,20}$/;
