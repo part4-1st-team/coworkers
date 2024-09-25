8adcf6c54102b94e28c22c9ea1069d535c786347
mxkxx <mxkxx@naver.com>
2024-09-25 14:52:29 +0900

    style: self close

--- src/components/modal/ModalTitle.tsx
+++ src/components/modal/ModalTitle.tsx
@@ -7,7 +7,7 @@ function ModalTitle({ title }: { title: string }) {
     <h2
       className='text-text-primary text-lg font-medium'
       dangerouslySetInnerHTML={{ __html: cleanTitle }}
-    ></h2>
+    />
   );
 }
 
