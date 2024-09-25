85b8b65ee393a9822d674e72b945bc52016d5349
mxkxx <mxkxx@naver.com>
2024-09-25 13:57:12 +0900

    refactor: modal title dompurify 추가

--- src/components/modal/ModalTitle.tsx
+++ src/components/modal/ModalTitle.tsx
@@ -1,5 +1,14 @@
+import dompurify from 'dompurify';
+
 function ModalTitle({ title }: { title: string }) {
-  return <h2 className='text-text-primary text-lg font-medium'>{title}</h2>;
+  const sanitizer = dompurify.sanitize;
+  const cleanTitle = sanitizer(title);
+  return (
+    <h2
+      className='text-text-primary text-lg font-medium'
+      dangerouslySetInnerHTML={{ __html: cleanTitle }}
+    ></h2>
+  );
 }
 
 export default ModalTitle;
