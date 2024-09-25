98cb48dae0450f982e33881e02dc58dae6542301
Seo-Hyang <82509465+Seo-Hyang@users.noreply.github.com>
2024-09-25 14:42:36 +0900

    feat/#29 group edit (#129)
    * feat: 그룹 수정 페이지 기능 구현, ROLE 상태에 따라 드롭다운 상태 변경
    
    * refactor: 그룹 생성하기 form 훅 수정, 토스트 처리 추가
    
    * feat: 그룹 탈퇴, 삭제 구분 구현
    
    * feat: 그룹 이미지 상태에 따라 미리보기에 보이도록 적용, 이미지 변경 적용
    
    * refactor: lint에 맞게 수정
    
    * refactor: header에 맞춰 main-container 스타일 적용

---
+++ src/containers/group/editGroup/EditGroup.tsx
@@ -0,0 +1,108 @@
+import { useState, useEffect } from 'react';
+import { useForm, Controller, SubmitHandler } from 'react-hook-form';
+import { useRouter } from 'next/router';
+import Input from '@/components/input/input';
+import Button from '@/components/button/button';
+import useToast from '@/components/toast/useToast';
+import { getGroup, patchGroup } from '@/services/GroupAPI';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import ImgUpload from '@/components/imgUpload/ImgUpload';
+
+interface FormState {
+  name: string;
+}
+
+function EditGroup() {
+  const { toast } = useToast();
+  const { control, handleSubmit, reset } = useForm<FormState>();
+  const [group, setGroup] = useState<Group | null>(null);
+  const [currentImage, setCurrentImage] = useState<string | null>(null);
+  const [prevImg, setPrevImg] = useState<string | undefined>();
+  const queryClient = useQueryClient();
+  const router = useRouter();
+  const { groupId } = router.query;
+
+  // 그룹 정보 가져오는 함수
+  const groupSetting = async () => {
+    if (groupId) {
+      const currentGroup = await getGroup(Number(groupId));
+      setGroup(currentGroup);
+      setPrevImg(currentGroup.image);
+      reset({ name: currentGroup.name });
+    }
+  };
+
+  const updateGroupMutation = useMutation({
+    mutationFn: async (name: string) => {
+      const data: PatchGroup = {};
+
+      if (currentImage !== null) {
+        data.image = currentImage;
+      }
+
+      if (group?.name !== name) {
+        data.name = name;
+      }
+
+      return patchGroup(Number(groupId), data);
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['group'],
+      });
+      toast('Success', '팀 설정이 업데이트되었습니다.');
+      router.push(`/group/${groupId}`);
+    },
+    onError: () => {
+      toast('Error', '팀 설정 업데이트에 실패했습니다.');
+    },
+  });
+
+  const handleChangeGroup: SubmitHandler<FormState> = (data) => {
+    const { name } = data;
+
+    updateGroupMutation.mutateAsync(name);
+  };
+
+  useEffect(() => {
+    groupSetting();
+  }, [groupId]);
+
+  return (
+    <div className='main-container '>
+      <div className='mx-16 tablet:mx-142 desktop:mx-430 mt-132 text-lg text-text-primary'>
+        <div className='w-full flex flex-col items-center gap-24'>
+          <div className='text-4xl'>팀 수정하기</div>
+          <form className='w-full' onSubmit={handleSubmit(handleChangeGroup)}>
+            <div className='w-64 mb-24'>
+              <div className='mb-12'>팀 프로필</div>
+              <ImgUpload prevImg={prevImg} setImgUrl={setCurrentImage} />
+            </div>
+            <div className='mb-40'>
+              <div className='mb-12'>팀 이름</div>
+              <Controller
+                name='name'
+                control={control}
+                render={({ field }) => (
+                  <Input
+                    placeholder='팀 이름을 입력해주세요.'
+                    value={field.value || ''}
+                    onChange={field.onChange}
+                  />
+                )}
+              />
+            </div>
+            <Button type='submit' color='primary' size='lg' className='w-full'>
+              수정하기
+            </Button>
+          </form>
+          <section className='w-full text-center font-normal'>
+            팀 이름은 회사명이나 모임 이름 등으로 설정하면 좋아요.
+          </section>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default EditGroup;
--- src/containers/group/groupId/groupTaskList/GroupPage.tsx
+++ src/containers/group/groupId/groupTaskList/GroupPage.tsx
@@ -1,7 +1,7 @@
 import useQueryParameter from '@/hooks/useQueryParameter';
 import { useEffect, useState } from 'react';
 import { getGroup } from '@/services/GroupAPI';
-
+import { getUser } from '@/services/userAPI';
 import EmptyGroup from './EmptyGroup';
 import GroupBar from './GroupBar';
 import GroupTask from './GroupTask';
@@ -17,6 +17,7 @@ function GroupPage() {
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [doneCount, setDoneCount] = useState<number>(0);
   const [totalCount, setTotalCount] = useState<number>(0);
+  const [isAdmin, setIsAdmin] = useState<boolean>(false);
 
   useEffect(() => {
     async function fetchData() {
@@ -28,6 +29,18 @@ function GroupPage() {
           setGroup(groupData);
           setGroupTaskLists(taskLists);
           setGroupMembers(members);
+
+          const userData = await getUser();
+          const userId = userData.id;
+
+          const userRole = members.find(
+            (member) => member.userId === userId,
+          )?.role;
+          if (userRole === 'ADMIN') {
+            setIsAdmin(true);
+          } else {
+            setIsAdmin(false);
+          }
         } catch (error) {
           console.error('Error fetching group data:', error);
         } finally {
@@ -51,7 +64,9 @@ function GroupPage() {
   return (
     <div className='w-full h-full bg-background-primary text-text-primary text-lg px-24'>
       <section className='w-full desktop:w-1200 desktop:mx-auto pt-24'>
-        <GroupBar>{group.name}</GroupBar>
+        <GroupBar groupId={groupId} groupName={group.name} isAdmin={isAdmin}>
+          {group.name}
+        </GroupBar>
         <GroupTask Lists={groupTaskLists} />
         <GroupReport doneCount={doneCount} totalCount={totalCount} />
         <GroupMembers Members={groupMembers} />
