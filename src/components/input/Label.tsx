b9f4a1a25d106e95bd99123e440a0de5d3d2de45
mxkxx1011 <100355178+mxkxx1011@users.noreply.github.com>
2024-09-28 14:36:02 +0900

    Update Label.tsx

--- src/components/input/Label.tsx
+++ src/components/input/Label.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from 'react';
 
 function Label({ id, children }: { id: string; children: ReactNode }) {
   return (
-     <label htmlFor={id} className='text-text-primary text-lg font-medium '>
+    <label htmlFor={id} className='text-text-primary text-lg font-medium '>
       {children}
     </label>
   );
