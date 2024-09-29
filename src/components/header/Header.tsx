8e6c8eee22bb6d5bbabaa9f4ff338b870ed2161b
Seo-Hyang <hyangaroma@gmail.com>
2024-09-30 03:58:59 +0900

    feat: 사이드 메뉴 기능 구현

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -5,17 +5,26 @@ import Link from 'next/link';
 import { useRouter } from 'next/router';
 import HeaderGroupDropdown from './HeaderGroupDropdown';
 import UserDropdown from './UserDropdown';
+import { useState } from 'react';
+import SideMenu from '../sidemenu/SideMenu';
 
 function Header() {
   const router = useRouter();
   const currentPath = router.pathname;
+  const [isSideMenuOpen, setIsSideMenuOpen] = useState(false);
+
+  const toggleSideMenu = () => {
+    setIsSideMenuOpen(!isSideMenuOpen);
+  };
 
   return (
     <div className='fixed top-0 left-0 border border-background-tertiary z-header w-full h-60 bg-background-secondary text-text-primary text-lg font-md px-16 tablet:px-24 flex items-center'>
       <div className='w-full desktop:w-1200 desktop:mx-auto flex'>
         <div className='flex items-center'>
-          <IconMenu className='w=24 mr-16  tablet:hidden' />
-
+          <IconMenu
+            className='w=24 mr-16 tablet:hidden'
+            onClick={toggleSideMenu}
+          />
           <Link href='/'>
             <LogoSmall className='desktop:hidden fill-brand-primary' />
             <LogoLarge className='hidden desktop:block fill-brand-primary' />
@@ -28,7 +37,6 @@ function Header() {
             <div className='flex items-center '>
               <div className='hidden ml-32 tablet:flex items-center gap-40'>
                 <HeaderGroupDropdown />
-
                 <Link href='/board' className='desktop:ml-40'>
                   자유게시판
                 </Link>
@@ -37,6 +45,7 @@ function Header() {
             <UserDropdown />
           </div>
         )}
+        {isSideMenuOpen && <SideMenu onClose={toggleSideMenu} />}
       </div>
     </div>
   );
