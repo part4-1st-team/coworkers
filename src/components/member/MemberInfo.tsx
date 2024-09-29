60f743d84d9a633899ef5eca9a5da32d715bdfa3
Seo-Hyang <hyangaroma@gmail.com>
2024-09-30 07:23:19 +0900

    refactor: 프로필 컴포넌트 적용

--- src/components/member/MemberInfo.tsx
+++ src/components/member/MemberInfo.tsx
@@ -1,25 +1,17 @@
 import React from 'react';
-import Image from 'next/image';
 import IconKebabSmall from '@/assets/icons/ic_kebab_small.svg';
 import useModalStore from '@/stores/ModalStore';
 import ProfileModal from '../modal/ProfileModal';
-
-// TODO 프로필 이미지 컴포넌트
+import ProfileImage from './ProfileImage';
 
 function MemberInfo({ member }: { member: IMember }) {
-  const defaultProfile: string = '/svgs/ic_member.svg';
   const { setModalOpen } = useModalStore();
-  const { userName, userEmail } = member;
+  const { userName, userEmail, userImage } = member;
 
   return (
     <div className='flex justify-between items-center w-fill h-73 rounded-16 px-24 py-20 bg-background-secondary'>
       <div className='flex flex-start gap-12'>
-        <Image
-          src={defaultProfile}
-          alt='프로필 이미지'
-          width={32}
-          height={32}
-        />
+        <ProfileImage userImage={userImage} size={32} />
         <div className='flex flex-col gap-2'>
           <b className='text-text-primary text-md'>{userName}</b>
           <p className='text-text-default text-xs'>{userEmail}</p>
