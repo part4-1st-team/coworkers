80df06d936420803eade63c48bd2a143a61e51b4
mxkxx <mxkxx@naver.com>
2024-09-30 03:23:45 +0900

    refactor: tooltip 적용해둔 곳 tooltipWrapper 컴포넌트로 변경

--- src/components/tooltip/TooltipWrapper.tsx
+++ src/components/tooltip/TooltipWrapper.tsx
@@ -1,21 +1,34 @@
+import clsx from 'clsx';
 import { ReactNode, useState } from 'react';
 
 interface ToolTipProps {
   children: ReactNode;
   message: string;
+  className?: string;
+  position?: string;
 }
 
-function TooltipWrapper({ children, message }: ToolTipProps) {
+function TooltipWrapper({
+  children,
+  message,
+  className = '',
+  position = 'top-[-200%]',
+}: ToolTipProps) {
   const [showTooltip, setShowTooltip] = useState<boolean>(false);
   return (
     <div
       onMouseEnter={() => setShowTooltip(true)} // 마우스가 버튼에 올라갔을 때
       onMouseLeave={() => setShowTooltip(false)} // 마우스가 버튼을 떠났을 때
-      className='relative'
+      className={clsx('relative', className)}
     >
       {children}
       {showTooltip && (
-        <div className='absolute top-[-200%] text-md text-text-secondary dark:text-text-secondary-dark  whitespace-nowrap p-10 bg-background-tertiary dark:bg-background-tertiary-dark shadow-md rounded-12'>
+        <div
+          className={clsx(
+            'absolute text-md text-text-secondary dark:text-text-secondary-dark  whitespace-nowrap p-10 bg-background-tertiary dark:bg-background-tertiary-dark shadow-md rounded-12',
+            position,
+          )}
+        >
           {message}
         </div>
       )}
--- src/containers/group/groupId/tasklist/TaskListAddPlusButton.tsx
+++ src/containers/group/groupId/tasklist/TaskListAddPlusButton.tsx
@@ -1,40 +1,29 @@
 import { IconPlus } from '@/assets/IconList';
 import TaskCreateModal from '@/components/modal/TaskCreateModal';
+import TooltipWrapper from '@/components/tooltip/TooltipWrapper';
 import useQueryParameter from '@/hooks/useQueryParameter';
 import useModalStore from '@/stores/ModalStore';
-import clsx from 'clsx';
-import { useState } from 'react';
 
 function TaskListAddPlusButton() {
-  const [showTooltip, setShowTooltip] = useState<boolean>(false);
-
   const { setModalOpen } = useModalStore();
 
   const { groupId } = useQueryParameter();
 
   return (
-    <div className='relative flex items-center justify-center'>
+    <TooltipWrapper
+      message='새로운 목록을 추가하시겠습니까?'
+      className='flex justify-center'
+      position='top-[-60%]'
+    >
       <button
         type='button'
         onClick={() => setModalOpen(<TaskCreateModal groupId={groupId} />)}
         className='shadow-md bg-brand-primary hover:bg-interaction-hover text-white size-60 tablet:size-80 rounded-12 flex items-center justify-center'
-        onMouseEnter={() => setShowTooltip(true)} // 마우스가 버튼에 올라갔을 때
-        onMouseLeave={() => setShowTooltip(false)} // 마우스가 버튼을 떠났을 때
         aria-label='새로운 목록 추가 버튼'
       >
         <IconPlus width={32} height={32} />
       </button>
-      {showTooltip && (
-        <p
-          className={clsx(
-            'absolute top-[-45px] left-0 w-fit whitespace-nowrap text-md font-medium p-10 rounded-12 shadow-md border border-border-primary dark:border-border-primary-dark',
-            'bg-background-tertiary dark:bg-background-tertiary-dark text-text-secondary dark:text-text-secondary-dark',
-          )}
-        >
-          새로운 목록을 추가하시겠습니까?
-        </p>
-      )}
-    </div>
+    </TooltipWrapper>
   );
 }
 
