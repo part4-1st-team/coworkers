2446a2e24ea26b683b6365a51f94accf73d313b1
Seo-Hyang <hyangaroma@gmail.com>
2024-09-30 07:24:11 +0900

    refactor: 프로필 컴포넌트 적용

--- src/components/modal/ProfileModal.tsx
+++ src/components/modal/ProfileModal.tsx
@@ -1,24 +1,34 @@
 import Button from '@/components/button/button';
 import Modal from './Modal';
+import useToast from '../toast/useToast';
+import useModalStore from '@/stores/ModalStore';
+import ProfileImage from '../member/ProfileImage';
 
 function ProfileModal({ member }: { member: IMember }) {
-  // const { userImage, userName, userEmail } = member;
-  const { userName, userEmail } = member;
+  const { userName, userEmail, userImage } = member;
+  const { toast } = useToast();
+  const { setModalClose } = useModalStore();
 
-  const handleEmailCopy = () => {
-    // NOTE: 이메일 카피하는 기능 추가
+  const handleEmailCopy = async () => {
+    try {
+      await navigator.clipboard.writeText(userEmail);
+      toast('Success', '이메일이 복사되었습니다.');
+      setModalClose();
+    } catch (error) {
+      toast('Error', '이메일 복사에 실패했습니다.');
+    }
   };
 
   return (
     <Modal.Close>
       <div className='px-16 pt-32 flex flex-col items-center gap-24'>
-        <div className='w-52 h-52 rounded-[9999px] bg-white' />
-        {/* TODO : 프로필 컴포넌트로 변경하기 */}
+        <ProfileImage userImage={userImage} size={52} />
+        {/* TODO : 텍스트 생략*/}
         <div className='text-center'>
-          <p className='text-text-primary text-md font-medium mb-[8px]'>
+          <p className='text-text-primary-dark text-md font-medium mb-8 overflow-hidden whitespace-nowrap text-ellipsis'>
             {userName}
           </p>
-          <p className='text-text-secondary text-xs font-normal '>
+          <p className='text-text-secondary-dark text-xs font-normal overflow-hidden whitespace-nowrap text-ellipsis'>
             {userEmail}
           </p>
         </div>
