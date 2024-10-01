d422de027c6e2d9b6ad33b3d19979b2aa5cfd9e7
mxkxx <mxkxx@naver.com>
2024-10-01 13:02:15 +0900

    feat: 캘린더 이전 날짜 선택 못하는 기능 추가 및 툴팁으로 기능 안내

--- src/components/calendar/Calendar.tsx
+++ src/components/calendar/Calendar.tsx
@@ -1,6 +1,9 @@
+import getMonthDay from '@/utils/getMonthDay';
+import { clsx } from 'clsx';
 import { forwardRef, ReactElement, ReactNode } from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
+import TooltipWrapper from '../tooltip/TooltipWrapper';
 
 interface ExampleCustomInputProps {
   value?: string;
@@ -13,14 +16,19 @@ export const CalendarInput = forwardRef<
   ExampleCustomInputProps
 >(({ value = 'Select date', onClick = () => {} }, ref) => {
   return (
-    <button
-      type='button'
-      className='w-336 h-48 text-left text-text-default bg-background-secondary px-16 py-15 rounded-xl leading-4 hover:ring-interaction-hover focus:ring-1'
-      onClick={onClick}
-      ref={ref} // ref를 전달
+    <TooltipWrapper
+      position='right-0 top-[-100%]'
+      message={`현재 날짜 이전은 선택이 불가능합니다.`}
     >
-      {value}
-    </button>
+      <button
+        type='button'
+        className='w-336 h-48 text-left text-text-default dark:text-text-default-dark bg-background-secondary dark:bg-background-secondary-dark px-16 py-15 rounded-xl leading-4 border border-border-primary dark:border-background-tertiary-dark focus:ring-1'
+        onClick={onClick}
+        ref={ref} // ref를 전달
+      >
+        {value}
+      </button>
+    </TooltipWrapper>
   );
 });
 
@@ -31,18 +39,35 @@ function Calendar({
   pickDate,
   setPickDate,
   format,
+  min = false,
 }: {
   trigger?: ReactElement;
   pickDate: Date;
   setPickDate: any;
   format?: string;
+  min?: boolean;
 }) {
+  const today = new Date();
+  today.setHours(0);
+  today.setMinutes(0);
+  today.setSeconds(0);
+  today.setMilliseconds(0);
+
+  const getDayClassName = (date: Date) => {
+    if (!min) return '';
+    return clsx({
+      'cursor-default pointer-events-none': date < today, // 오늘 이전 날짜는 비활성화
+    });
+  };
+
   return (
     <DatePicker
       selected={pickDate}
       onChange={(nextDate: Date | null) => setPickDate(nextDate)}
       customInput={trigger}
       dateFormat={format ? 'dd일' : 'yyyy년 MM월 dd일'}
+      minDate={min ? today : undefined}
+      dayClassName={getDayClassName}
     />
   );
 }
