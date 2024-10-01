53b414763e2334af7e229ae817bd91d5f75ee542
mxkxx <mxkxx@naver.com>
2024-10-01 13:19:08 +0900

    refactor: half 페이지의 완료하기 버튼 분리 및 툴팁 수정

--- src/components/tooltip/TooltipWrapper.tsx
+++ src/components/tooltip/TooltipWrapper.tsx
@@ -3,9 +3,10 @@ import { ReactNode, useState } from 'react';
 
 interface ToolTipProps {
   children: ReactNode;
-  message: string;
+  message: ReactNode;
   className?: string;
   position?: string;
+  messageClassName?: string;
 }
 
 function TooltipWrapper({
@@ -13,20 +14,43 @@ function TooltipWrapper({
   message,
   className = '',
   position = 'top-[-200%]',
+  messageClassName = '',
 }: ToolTipProps) {
   const [showTooltip, setShowTooltip] = useState<boolean>(false);
+
+  let timeoutRef: any = null; // 타이머 참조를 저장할 변수
+  const TOOLTIP_TIME = 200;
+
+  const handleMouseEnter = () => {
+    clearTimeout(timeoutRef);
+    setShowTooltip(true);
+  };
+
+  const handleMouseLeave = () => {
+    timeoutRef = setTimeout(() => {
+      setShowTooltip(false);
+    }, TOOLTIP_TIME);
+  };
+
+  const handleMessageMouseEnter = () => {
+    clearTimeout(timeoutRef); // 리스트에 마우스가 들어오면 타이머 클리어
+  };
+
   return (
     <div
-      onMouseEnter={() => setShowTooltip(true)} // 마우스가 버튼에 올라갔을 때
-      onMouseLeave={() => setShowTooltip(false)} // 마우스가 버튼을 떠났을 때
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
       className={clsx('relative', className)}
     >
       {children}
       {showTooltip && (
         <div
+          onMouseEnter={handleMessageMouseEnter}
+          onMouseLeave={handleMouseLeave}
           className={clsx(
             'absolute text-md text-text-secondary dark:text-text-secondary-dark  whitespace-nowrap p-10 bg-background-tertiary dark:bg-background-tertiary-dark shadow-md rounded-12',
             position,
+            messageClassName,
           )}
         >
           {message}
