a7cb9ef5ad75d34ec39c62df14329b616d255bad
mxkxx <mxkxx@naver.com>
2024-10-03 23:45:01 +0900

    style: 아이콘 추가 및 아이콘 타입 지정

--- src/components/icon/Google.tsx
+++ src/components/icon/Google.tsx
@@ -1,7 +1,7 @@
 import IconGoogle from '@/assets/images/img_google.png';
 import Image from 'next/image';
 
-function GoogleIcon({ size }: { size: number }) {
+function GoogleIcon({ size }: IconProps) {
   return <Image src={IconGoogle} width={size} height={size} alt='카카오톡' />;
 }
 
--- src/components/icon/Kakao.tsx
+++ src/components/icon/Kakao.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import IconKaKao from '@/assets/images/img_kakaotalk.png';
 
-function KakaoIcon({ size }: { size: number }) {
+function KakaoIcon({ size }: IconProps) {
   return <Image src={IconKaKao} width={size} height={size} alt='구글' />;
 }
 
