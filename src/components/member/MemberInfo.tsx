544f0baa0af378957de4c25e0bf42d9b2c06f2a6
Seo-Hyang <hyangaroma@gmail.com>
2024-10-04 11:40:37 +0900

    feat: 멤버 목록에 그룹장 표시 추가

--- src/components/member/MemberInfo.tsx
+++ src/components/member/MemberInfo.tsx
@@ -3,10 +3,11 @@ import IconKebabSmall from '@/assets/icons/ic_kebab_small.svg';
 import useModalStore from '@/stores/ModalStore';
 import ProfileModal from '../modal/ProfileModal';
 import ProfileImage from './ProfileImage';
+import CrownIcon from '../icon/Crown';
 
 function MemberInfo({ member }: { member: IMember }) {
   const { setModalOpen } = useModalStore();
-  const { userName, userEmail, userImage } = member;
+  const { role, userName, userEmail, userImage } = member;
 
   return (
     <div className='flex items-center justify-between w-fill h-73 rounded-16 px-24 py-20 shadow-md before:bg-background-secondary dark:bg-background-secondary-dark'>
@@ -17,8 +18,9 @@ function MemberInfo({ member }: { member: IMember }) {
       >
         <ProfileImage userImage={userImage} size={32} />
         <div className='flex flex-col items-start justify-center w-fill h-33 gap-2'>
-          <b className='relative inline-block text-text-primary dark:text-text-primary-dark text-md'>
+          <b className='relative flex items-center gap-4 text-text-primary dark:text-text-primary-dark text-md'>
             {userName}
+            {role === 'ADMIN' ? <CrownIcon /> : <></>}
           </b>
           <p className='hidden tablet:inline-block text-text-default dark:text-text-default-dark text-xs'>
             {userEmail}
