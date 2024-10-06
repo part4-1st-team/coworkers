3feaefb14beef91d79347e1a803b2e026a96c49f
mxkxx <mxkxx@naver.com>
2024-10-07 03:54:54 +0900

    refactor: eslint 수정

--- src/components/switch/ThemeSwitch.tsx
+++ src/components/switch/ThemeSwitch.tsx
@@ -4,20 +4,25 @@ import { twMerge } from 'tailwind-merge';
 function ThemeSwitch() {
   const { theme, switchTheme } = useTheme();
   return (
-    <label
-      className={twMerge(
-        'switch',
-        theme === 'dark' ? 'switch-dark' : 'switch-light',
-      )}
-    >
-      <input
-        type='checkbox'
-        className='appearance-none peer hidden'
-        onClick={switchTheme}
-        checked={theme === 'dark'}
-      />
-      <span className='slider'></span>
-    </label>
+    <>
+      {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+      <label
+        htmlFor='switch'
+        className={twMerge(
+          'switch',
+          theme === 'dark' ? 'switch-dark' : 'switch-light',
+        )}
+      >
+        <input
+          id='switch'
+          type='checkbox'
+          className='appearance-none peer hidden'
+          onClick={switchTheme}
+          checked={theme === 'dark'}
+        />
+        <span className='slider' />
+      </label>
+    </>
   );
 }
 
