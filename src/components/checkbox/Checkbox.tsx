b30d2e873e362f981e94d9836ef6bf3b2391f3af
mxkxx <mxkxx@naver.com>
2024-10-07 01:08:04 +0900

    style: 체크박스 스타일 수정

--- src/components/checkbox/Checkbox.tsx
+++ src/components/checkbox/Checkbox.tsx
@@ -1,4 +1,4 @@
-import { IconCheckboxActive, IconCheckboxDefault } from '@/assets/IconList';
+import { twMerge } from 'tailwind-merge';
 
 interface CheckboxProps {
   checked: boolean;
@@ -7,9 +7,18 @@ interface CheckboxProps {
 
 function Checkbox({ handleClick, checked }: CheckboxProps) {
   return (
-    <button type='button' onClick={handleClick}>
-      {checked ? <IconCheckboxActive /> : <IconCheckboxDefault />}
-    </button>
+    <input
+      onClick={handleClick}
+      type='checkbox'
+      className={twMerge(
+        'appearance-none size-16 rounded-4 border border-border-primary shadow-md dark:border-border-priamry-dark',
+        'checked:bg-brand-primary checked:border-0',
+        `checked:bg-[url('/svgs/ic_check.svg')] bg-no-repeat bg-center`,
+        'focus:outline-none focus:ring-offset-0 focus:ring-1 focus:ring-brand-secondary',
+        'hover:shadow-[0_8px_20px_rgba(93,115,232,0.5)]',
+      )}
+      checked={checked}
+    />
   );
 }
 
