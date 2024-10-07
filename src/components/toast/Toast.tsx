07682360865ef60d7a30ec19b25bc34f5ed3067b
mxkxx1011 <100355178+mxkxx1011@users.noreply.github.com>
2024-10-07 14:23:21 +0900

    style/#188 토스트 컴포넌트 크기 수정 및 모바일/데스크탑 위치 수정 (#196)
    * style: 토스트 컴포넌트 크기 수정 및 모바일/데스크탑 위치 수정
    
    * Update ToastItem.tsx

--- src/components/toast/Toast.tsx
+++ src/components/toast/Toast.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import { createPortal } from 'react-dom';
+import { twMerge } from 'tailwind-merge';
 import ToastItem from './ToastItem';
 import useToastStore from './ToastStore';
 
@@ -25,7 +26,13 @@ function Toast() {
   return (
     <>
       {createPortal(
-        <div className='fixed top-70 right-30 flex flex-col gap-5'>
+        <div
+          className={twMerge(
+            'flex flex-col gap-8 w-full tablet:w-[30%] items-center',
+            'fixed top-80 left-1/2 translate-x-[-50%]',
+            'tablet:top-80 tablet:right-20 tablet:bottom-auto tablet:left-auto tablet:translate-x-0',
+          )}
+        >
           {toastList.map((toast) => (
             <ToastItem key={toast.id} toast={toast} />
           ))}
