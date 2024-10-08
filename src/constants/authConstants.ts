9cae61902deb15992b4f62bf2b95fc08ae0cbfd4
ohjohwan <whghks1020@naver.com>
2024-10-08 13:32:35 +0900

    refactor: 카카오 리다이렉트 uri 배포 링크로 수정

--- src/constants/authConstants.ts
+++ src/constants/authConstants.ts
@@ -1,4 +1,4 @@
 export const GOOGLE_REDIRECT_URI = 'http://localhost:3000/oauth/google'; // 구글 로그인 후 리디렉션 URI
 export const GOOGLE_CLIENT_ID = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID; // 구글 클라우드 콘솔에서 발급받은 클라이언트 ID
-export const KAKAO_REDIRECT_URI = 'http://localhost:3000/oauth/kakao'; // 카카오 로그인 후 리디렉션 URI
+export const KAKAO_REDIRECT_URI = 'https://coworkers-1.netlify.app/oauth/kakao'; // 카카오 로그인 후 리디렉션 URI
 export const KAKAO_CLIENT_ID = process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID; // 카카오 개발자 콘솔에서 발급받은 클라이언트 ID
