1c6e0620768250960fc9e460289bbd594a0da2f1
ohjohwan <whghks1020@naver.com>
2024-10-08 13:07:06 +0900

    feat: queryClient를 사용해 user 쿼리 갱신 설정

--- src/stores/userStore.ts
+++ src/stores/userStore.ts
@@ -2,9 +2,9 @@
 import { create } from 'zustand';
 import { persist } from 'zustand/middleware';
 import { setTokenCookies, removeTokenCookies } from '@/utils/cookieUtils';
+import { useQueryClient } from '@tanstack/react-query';
 
 // user 인터페이스 추가
-
 interface UserStoreState {
   user: BasicUser | null;
   accessToken: string | null;
@@ -24,53 +24,70 @@ interface UserStoreState {
 
 const useUserStore = create<UserStoreState>()(
   persist(
-    (set) => ({
-      user: null,
-      accessToken: null,
-      isLoggedIn: false,
-      refreshToken: null,
-      isSocialLogin: null,
-      setLogin: (
-        newUser: User,
-        aToken: string,
-        rToken: string,
-        isSocialLogin = null, // 인자 3개 전달 오류 해결을 위해 기본값 설정
-      ) => {
-        set({
-          user: newUser,
-          isLoggedIn: true,
-          accessToken: aToken,
-          refreshToken: rToken,
-          isSocialLogin, // 간편 로그인 여부 저장
-        });
-        // 쿠키에 토큰 저장
-        setTokenCookies(aToken, rToken);
-      },
-      // 로그아웃 메소드
-      setLogout: () => {
-        set({
-          user: null,
-          isLoggedIn: false,
-          accessToken: null,
-          refreshToken: null,
-          isSocialLogin: null, // 로그아웃 시 간편 로그인 여부 초기화
-        });
-        // 쿠키에서 토큰 삭제
-        removeTokenCookies();
-      },
-      setToken: (aToken: string, rToken: string) => {
-        set({
-          accessToken: aToken,
-          refreshToken: rToken,
-        });
-      },
-      removeToken: () => {
-        set({
-          accessToken: null,
-          refreshToken: null,
-        });
-      },
-    }),
+    (set) => {
+      const queryClient = useQueryClient();
+
+      return {
+        user: null,
+        accessToken: null,
+        isLoggedIn: false,
+        refreshToken: null,
+        isSocialLogin: null,
+
+        // 로그인 메소드
+        setLogin: (
+          newUser: User,
+          aToken: string,
+          rToken: string,
+          isSocialLogin = null, // 인자 3개 전달 오류 해결을 위해 기본값 설정
+        ) => {
+          set({
+            user: newUser,
+            isLoggedIn: true,
+            accessToken: aToken,
+            refreshToken: rToken,
+            isSocialLogin, // 간편 로그인 여부 저장
+          });
+          // 쿠키에 토큰 저장
+          setTokenCookies(aToken, rToken);
+
+          // useUser 쿼리 갱신
+          queryClient.invalidateQueries({ queryKey: ['user'] }); // 쿼리 갱신
+        },
+
+        // 로그아웃 메소드
+        setLogout: () => {
+          set({
+            user: null,
+            isLoggedIn: false,
+            accessToken: null,
+            refreshToken: null,
+            isSocialLogin: null, // 로그아웃 시 간편 로그인 여부 초기화
+          });
+          // 쿠키에서 토큰 삭제
+          removeTokenCookies();
+
+          // useUser 쿼리 갱신
+          queryClient.invalidateQueries({ queryKey: ['user'] }); // 로그아웃 후 쿼리 갱신을 통해 캐시를 무효화
+        },
+
+        // 토큰 업데이트 메소드
+        setToken: (aToken: string, rToken: string) => {
+          set({
+            accessToken: aToken,
+            refreshToken: rToken,
+          });
+        },
+
+        // 토큰 삭제 메소드
+        removeToken: () => {
+          set({
+            accessToken: null,
+            refreshToken: null,
+          });
+        },
+      };
+    },
     {
       name: 'User',
     },
